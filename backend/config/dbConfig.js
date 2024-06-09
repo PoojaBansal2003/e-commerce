d93dd6d002c934673c2df82f133850ca76dced84
poojabansal2003 <poojabansal1307@gmail.com>
2024-06-09 16:24:28 +0530

    Your commit message here

---
+++ backend/config/dbConfig.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+    MONGODB_URI: 'mongodb+srv://poojabansal1307:IjUOKDOkAWwvUPdL@cluster0.kjcdkvk.mongodb.net/', // MongoDB connection URI
+    PORT: 5000, 
+    JWT_SECRET: 'poojabansal1307', // JWT secret for authentication
+  };
+  
